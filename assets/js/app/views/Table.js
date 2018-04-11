c0ba9a391cd8a257ce729ce0de4feb18661f6a19
Breno Franca <bre.sfranca@gmail.com>
2018-04-11 18:06:18 -0300

    Adaptando landing para o assignment 3

---
+++ assets/js/app/views/Table.js
@@ -0,0 +1,31 @@
+class TableView extends Views {
+
+  constructor (element){
+    super(element);
+  }
+
+  template (model){
+    return `
+      <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Name</th>
+              <th>E-mail</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            ${Object.entries(model).map(data => `
+              <tr>
+                <td>${data[1]._currentdate}</td>
+                <td>${data[1]._name}</td>
+                <td>${data[1]._email}</td>
+              </tr>
+            `).join('')}
+          </tbody>
+      </table>
+    `
+  }
+
+}
